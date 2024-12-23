0961f205a91c99fc6ff59fd3abbc6d6129b4aa58
HyperCoder-57 <ahernandezt2101@alumno.ipn.mx>
2024-12-23 17:25:36 -0600

    segundo commit

--- app/src/main/java/com/stark/dogboard/view/LoginActivity.kt
+++ app/src/main/java/com/stark/dogboard/view/LoginActivity.kt
@@ -1,22 +1,23 @@
 package com.stark.dogboard.view
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.Observer
-import com.stark.dogboard.R
 import com.stark.dogboard.databinding.ActivityLoginBinding
 import com.stark.dogboard.viewmodel.LoginViewModel
+
+
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val loginViewModel: LoginViewModel by viewModels()
+
+    private val username = "admin"
+    private val email = "correo@ejemplo.com"
+    private val password = "admin"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,21 +28,26 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun autenticarUsuario() {
-        val username = "admin"
-        val email = "correo@ejemplo.com"
-        val password = "admin"
 
-        loginViewModel.login(username, email, password).observe(this) { response ->
-            if (response != null) {
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        if ((binding.editTextUsuario.text.toString()
+                .trim() == username || binding.editTextUsuario.text.toString()
+                .trim() == email) && binding.editTextPassword.text.toString().trim() == password
+        )
+
+            loginViewModel.login(username, email, password).observe(this) { response ->
+                if (response != null) {
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    startActivity(intent)
+                } else {
+
+                    AlertDialog.Builder(this).setTitle("Error")
+                        .setMessage("No se pudo cargar la foto de perfil")
+                        .setPositiveButton("OK", null).show()
                 }
-                startActivity(intent)
-                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Houston, tenemos un problema!", Toast.LENGTH_SHORT).show()
             }
-        }
+
     }
 
     private fun initListeners() {
@@ -58,9 +64,10 @@ class LoginActivity : AppCompatActivity() {
     private fun initObservers() {
         loginViewModel.isBtnEnabled.observe(this) { isEnabled ->
             binding.btnIngresar.isEnabled = isEnabled
-            if(isEnabled){
-               // binding.btnIngresar.setBackgroundColor("@Color")
-            }else {
+
+            if (isEnabled) {
+                // binding.btnIngresar.setBackgroundColor("@Color")
+            } else {
 
             }
         }
